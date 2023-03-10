99dddcb0e3eca7969c51cffeaae8553edf7e2ba6
Ebi Ganranwei <ebiganny@gmail.com>
2023-03-10 13:18:05 +0100

    added a commet to Fikayos cloned repo'

--- greetings.py
+++ greetings.py
@@ -4,3 +4,4 @@ name = input("What is your name? ")
 hour = datetime.now().time().hour
 greeting = "Good morning" if 4 <= hour < 12 else "Good afternoon" if 12 <= hour < 16 else "Good night"
 print(f"{greeting}, {name}!")
+#Chii to you all
